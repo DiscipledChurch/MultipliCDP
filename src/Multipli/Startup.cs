9e80c5ca20141100952a37f68473da402e591979
Joshua Davis <me@jdav.is>
2016-01-14 19:08:14 -0500

    Renamed project from ChDS to MultipliChMS

--- src/Multipli/Startup.cs
+++ src/Multipli/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ChDS
+namespace Multipli
 {
     public class Startup
     {
