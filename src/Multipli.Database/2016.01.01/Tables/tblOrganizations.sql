4dd48c3c4d071236421fc821dd6977f875c50ddb
Joshua Davis <me@jdav.is>
2016-02-07 21:07:27 -0500

    added EF for data modeling, added FK for tying accounts to people, made customUrl not required

--- src/Multipli.Database/2016.01.01/Tables/tblOrganizations.sql
+++ src/Multipli.Database/2016.01.01/Tables/tblOrganizations.sql
@@ -3,6 +3,6 @@
 	[Id] BIGINT NOT NULL,
 	[Name] NVARCHAR(1024) NOT NULL,
 	[Hostname] NVARCHAR(15) NOT NULL,
-	[CustomUrl] NVARCHAR(1024) NOT NULL,
+	[CustomUrl] NVARCHAR(1024) NULL,
 	CONSTRAINT [PK_tblOrganizations] PRIMARY KEY ([Id] ASC)
 )
