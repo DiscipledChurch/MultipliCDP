57f225bc53ed7b79e267e4049c263120490e0c92
Joshua Davis <joshua.davis@sstid.com>
2016-04-14 23:01:53 -0400

    updated organizations table, added locations table, removed families index

---
+++ src/Multipli.Database/Tables/tblLocations.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblLocations]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL,
+	[OrganizationId] UNIQUEIDENTIFIER NOT NULL,
+	[Name] NVARCHAR(1024) NOT NULL,
+	[Hostname] NVARCHAR(15) NULL,
+	[CustomUrl] NVARCHAR(1024) NULL,
+	[IsAuthorized] BIT NULL,
+	CONSTRAINT [PK_tblLocations] PRIMARY KEY ([Id], [OrganizationId] ASC)
+)
--- src/Multipli.Database/Tables/tblOrganizations.sql
+++ src/Multipli.Database/Tables/tblOrganizations.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[tblOrganizations]
 (
-	[Id] BIGINT NOT NULL,
+	[Id] UNIQUEIDENTIFIER NOT NULL,
 	[Name] NVARCHAR(1024) NOT NULL,
 	[Hostname] NVARCHAR(15) NOT NULL,
 	[CustomUrl] NVARCHAR(1024) NULL,
+	[IsAuthorized] BIT NOT NULL,
 	CONSTRAINT [PK_tblOrganizations] PRIMARY KEY ([Id] ASC)
 )
