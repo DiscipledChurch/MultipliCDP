5945cc474f76d8eb6c75ebec8c39635a7923b1ce
Joshua Davis <me@jdav.is>
2016-04-16 13:51:06 -0400

    added authentication table (#8)

---
+++ src/Multipli.Database/Tables/tblAuthentications.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tblAuthentications]
+(
+	[PersonId] UNIQUEIDENTIFIER NOT NULL,
+	[Hash] NVARCHAR(128) NOT NULL,
+	[Salt] NVARCHAR(128) NOT NULL,
+	[ConfirmGuid] NVARCHAR(100) NULL,
+	[Confirmed] BIT NOT NULL DEFAULT 0,
+	[IsAuthorized] BIT NOT NULL DEFAULT 0,
+	[IsDeleted] BIT NOT NULL DEFAULT 0,
+	[CreateDate] DATETIME NOT NULL,
+	[LastLogin] DATETIME NULL,
+	CONSTRAINT [PK_tblAuthentications] PRIMARY KEY ([PersonId] ASC),
+	CONSTRAINT [FK_tblAuthentications_PersonId_tblPeople_Id] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[tblPeople]([Id])
+)
