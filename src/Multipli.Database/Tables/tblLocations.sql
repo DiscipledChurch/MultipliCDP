b058f7d1be72248c04b4392108f68fc722e5c419
Joshua Davis <me@jdav.is>
2016-04-16 20:09:43 -0400

    finished locations table

--- src/Multipli.Database/Tables/tblLocations.sql
+++ src/Multipli.Database/Tables/tblLocations.sql
@@ -5,6 +5,9 @@
 	[Name] NVARCHAR(1024) NOT NULL,
 	[Hostname] NVARCHAR(15) NULL,
 	[CustomUrl] NVARCHAR(1024) NULL,
-	[IsAuthorized] BIT NULL,
-	CONSTRAINT [PK_tblLocations] PRIMARY KEY ([Id], [OrganizationId] ASC)
+	[IsAuthorized] BIT NOT NULL DEFAULT 1,
+	CONSTRAINT [PK_tblLocations] PRIMARY KEY NONCLUSTERED ([Id], [OrganizationId] ASC),
+	CONSTRAINT [FK_tblLocations_OrganizationId_tblOrganizations_Id] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[tblOrganizations]([Id]),
+	CONSTRAINT [UC_tblLocations_Hostname] UNIQUE ([Hostname]),
+	CONSTRAINT [UC_tblLocations_CustomUrl] UNIQUE ([CustomUrl])
 )
