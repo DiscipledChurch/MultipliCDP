f85ef2d8b1884308ed35939b6cafe4acd4672655
Joshua Davis <me@jdav.is>
2016-04-16 20:00:04 -0400

    completed organizations table

--- src/Multipli.Database/Tables/tblOrganizations.sql
+++ src/Multipli.Database/Tables/tblOrganizations.sql
@@ -4,6 +4,8 @@
 	[Name] NVARCHAR(1024) NOT NULL,
 	[Hostname] NVARCHAR(15) NOT NULL,
 	[CustomUrl] NVARCHAR(1024) NULL,
-	[IsAuthorized] BIT NOT NULL,
-	CONSTRAINT [PK_tblOrganizations] PRIMARY KEY ([Id] ASC)
+	[IsAuthorized] BIT NOT NULL DEFAULT 0,
+	CONSTRAINT [PK_tblOrganizations] PRIMARY KEY ([Id] ASC),
+	CONSTRAINT [UC_tblOrganizations_Hostname] UNIQUE ([Hostname]),
+	CONSTRAINT [UC_tblOrganizations_CustomUrl] UNIQUE ([CustomUrl])
 )
