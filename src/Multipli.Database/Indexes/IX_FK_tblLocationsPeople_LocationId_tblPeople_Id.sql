70858e21152a23795f46a4009fe9a3c5ba4c3367
Joshua Davis <me@jdav.is>
2016-05-15 17:10:00 -0400

    added location & people join table with indexes

---
+++ src/Multipli.Database/Indexes/IX_FK_tblLocationsPeople_LocationId_tblPeople_Id.sql
@@ -0,0 +1,3 @@
+﻿CREATE NONCLUSTERED INDEX [IX_FK_tblLocationPeople_LocationId_tblPeople_Id]
+	ON [dbo].[tblLocationPeople]
+	(PersonId)
---
+++ src/Multipli.Database/Tables/tblLocationPeople.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tblLocationPeople]
+(
+	[LocationId] UNIQUEIDENTIFIER NOT NULL,
+	[PersonId] UNIQUEIDENTIFIER NOT NULL,
+	CONSTRAINT [PK_tblLocationPeople_LocationId] PRIMARY KEY ([LocationId] ASC),
+	CONSTRAINT [FK_tblLocationPeople_LocationId_tblLocations_Id] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[tblLocations]([Id]),
+	CONSTRAINT [FK_tblLocationPeople_LocationId_tblPeople_Id] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[tblPeople]([Id])
+)
