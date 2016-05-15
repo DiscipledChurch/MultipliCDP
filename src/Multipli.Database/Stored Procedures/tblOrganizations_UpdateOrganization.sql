cb5960bf66bc85639c521df747bd5c4daf668cf1
Joshua Davis <me@jdav.is>
2016-05-15 18:18:23 -0400

    update organization (#10)

---
+++ src/Multipli.Database/Stored Procedures/tblOrganizations_UpdateOrganization.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[tblOrganizations_UpdateOrganization]
+	@Id			UNIQUEIDENTIFIER,
+	@Name		NVARCHAR(1024),
+	@Hostname	NVARCHAR(25)
+AS
+	UPDATE [dbo].[tblOrganizations]
+	SET
+		Name = @Name
+	   ,Hostname = @Hostname
+	WHERE
+		Id = @Id 
