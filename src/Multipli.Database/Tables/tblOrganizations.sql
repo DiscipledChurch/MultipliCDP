a536fd80ea70c9a71409142746101b233a2838b5
Joshua Davis <me@jdav.is>
2016-05-15 18:05:03 -0400

    set organization to be automatically authorized

--- src/Multipli.Database/Tables/tblOrganizations.sql
+++ src/Multipli.Database/Tables/tblOrganizations.sql
@@ -4,7 +4,7 @@
 	[Name] NVARCHAR(1024) NOT NULL,
 	[Hostname] NVARCHAR(25) NOT NULL,
 	[CustomUrl] NVARCHAR(1024) NULL,
-	[IsAuthorized] BIT NOT NULL DEFAULT 0,
+	[IsAuthorized] BIT NOT NULL DEFAULT 1,
 	CONSTRAINT [PK_tblOrganizations] PRIMARY KEY ([Id] ASC),
 	CONSTRAINT [UC_tblOrganizations_Hostname] UNIQUE ([Hostname]),
 	CONSTRAINT [UC_tblOrganizations_CustomUrl] UNIQUE ([CustomUrl])
