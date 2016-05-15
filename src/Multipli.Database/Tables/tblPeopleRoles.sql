21fa3c21043fbc50143d3577bc7b5bfc0de85edb
Joshua Davis <me@jdav.is>
2016-05-15 17:34:35 -0400

    removed 'accounts' table, refactored roles join table to reference 'people' table

---
+++ src/Multipli.Database/Tables/tblPeopleRoles.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[tblPeopleRoles]
+(
+	[PersonId] UNIQUEIDENTIFIER NOT NULL,
+	[LocationId] UNIQUEIDENTIFIER NOT NULL,
+	[RoleId] SMALLINT NOT NULL,
+	CONSTRAINT [PK_tblPeopleRoles] PRIMARY KEY ([PersonId], [LocationId] ASC),
+	CONSTRAINT [FK_tblPeopleRoles_PersonId_tblPeople_Id] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[tblPeople]([Id]),
+	CONSTRAINT [FK_tblPeopleRoles_LocationId_tblLocations_Id] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[tblLocations]([Id]),
+	CONSTRAINT [FK_tblPeopleRoles_RoleId_tblRoles_Id] FOREIGN KEY ([RoleId]) REFERENCES [dbo].[tblRoles]([Id])
+)
