5896856d21f16ba28c9ffda277928cc40fca066d
Joshua Davis <me@jdav.is>
2016-05-15 18:13:38 -0400

    inserts new organization (#10)

---
+++ src/Multipli.Database/Stored Procedures/tblOrganizations_InsertOrganization.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[tblOrganizations_InsertOrganization]
+	@Name NVARCHAR(1024),
+	@Hostname NVARCHAR(25)
+AS
+	INSERT INTO [dbo].[tblOrganizations] (
+		Id,
+		Name,
+		Hostname,
+		CreatedDate
+		)
+	SELECT 
+		NEWID()
+	   ,@Name
+	   ,@Hostname
+	   ,GETUTCDATE();
+
