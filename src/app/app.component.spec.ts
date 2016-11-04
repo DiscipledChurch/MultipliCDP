9178809337ec9649d44f330e763e29334bcc31ab
Joshua Davis <me@jdav.is>
2016-11-04 17:35:46 -0400

    fixed h1 tag contents

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,7 +18,7 @@ describe('App: Multipli', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'code coverage works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('code coverage works!');
@@ -28,6 +28,6 @@ describe('App: Multipli', () => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('code coverage works!');
   }));
 });
