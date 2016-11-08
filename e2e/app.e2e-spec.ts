6a40e8894a0ddb8f09791bb16112fe94aeefee0e
Joshua Davis <me@jdav.is>
2016-11-08 10:41:23 -0500

    added async to beforeEach

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -3,8 +3,9 @@ import { MultipliPage } from './app.po';
 describe('multipli App', function() {
   let page: MultipliPage;
 
-  beforeEach(() => {
+  beforeEach((done) => {
     page = new MultipliPage();
+    done();
   });
 
   it('should display message saying app works', (done) => {
