53752d9e43851093db1920e357cf1e0353484c02
Joshua Davis <me@jdav.is>
2016-11-09 09:29:09 -0500

    another test

--- protractor.sauce.conf.js
+++ protractor.sauce.conf.js
@@ -58,8 +58,9 @@ exports.config = {
         browser.getSession().then(function(session) {
 
             var exec = require('child_process').exec;
-            var cmd = 'curl -X PUT -s -d \'{"name": "' + result.fullName + '", "passed": ' + (result.status == 'passed' ? 'true' : 'false') + ', "custom-data": {"' + result + '"} }\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + session.getId();
+            var cmd = 'curl -X PUT -s -d \'{"name": "' + result.fullName + '", "passed": ' + (result.status == 'passed' ? 'true' : 'false') + ', "custom-data": { "message": "' + result.failedExpectations[0].message + '"} }\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + session.getId();
 console.log(JSON.stringify(result));
+console.log(result.failedExpectations[0].message);
 //            var cmd = 'curl -X PUT -s -d \'{"name": "' + result.fullName + '", "passed": false, "custom-data": ' + JSON.stringify(result) + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + session.getId();
 
             exec(cmd, function(error, stdout, stderr) {
