6503600a3730c13c561fb8a69c734b8ed262075b
Joshua Davis <me@jdav.is>
2016-11-14 20:53:58 -0500

    added metadata for organization and location (#38)

--- server/models/location.js
+++ server/models/location.js
@@ -1,11 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var Address = require('./address');
+var PhoneNumber = require('./phoneNumber');
+
 var LocationSchema = new Schema({
+    name: { type: String, required: true },
     hostname: { type: String },
     customUrl: { type: String },
     isAuthorized: { type: Boolean, required: true },
-    createdDate: { type: Date, required: true }
+    createdDate: { type: Date, required: true },
+    addresses: { type: [Address] },
+    phoneNumbers: { type: [PhoneNumber] }
 });
 
 module.exports = mongoose.model('Location', LocationSchema);
--- server/models/organization.js
+++ server/models/organization.js
@@ -1,12 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var Address = require('./address');
+var PhoneNumber = require('./phoneNumber');
+var Location = require('./location');
+
 var OrganizationSchema = new Schema({
     name: { type: String, required: true },
     hostname: { type: String },
     customUrl: { type: String },
     isAuthorized: { type: Boolean, required: true },
-    createdDate: { type: Date, required: true }
+    createdDate: { type: Date, required: true },
+    addresses: { type: [Address] },
+    phoneNumbers: { type: [PhoneNumber] },
+    locations: { type: [Location] }
 });
 
 module.exports = mongoose.model('Organization', OrganizationSchema);
