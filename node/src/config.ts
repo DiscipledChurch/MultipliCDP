765a41ced09b321a19136f06b5b6dcf26210d24f
Joshua Davis <joshua@localhost.localdomain>
2017-01-05 09:29:56 -0800

    added typescript rewrite of config.js

---
+++ node/src/config.ts
@@ -0,0 +1,17 @@
+class Config {
+    public port: number;
+    public mongodb: string;
+    public secret: string;
+    public host: string;
+    public masterKey: string;
+
+    constructor() {
+        this.port = process.env.PORT || 8080;
+        this.mongodb = '';
+        this.secret = '';
+        this.host = '';
+        this.masterKey = '';
+    }
+}
+
+export = Config;
